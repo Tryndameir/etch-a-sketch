93c34478dcb6ab744f063e5465675ac037b35ca3
Tryndameir <andrewdinho99@gmail.com>
2023-07-24 04:22:29 -0400

    created proper 16x16 grid

--- myScript.js
+++ myScript.js
@@ -7,11 +7,3 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(div);
     }
 }
-
-/*const div = document.querySelector('.box');
-for (let i = 0; i < 16; i++) {
-    const div2 = document.createElement('div');
-    div2.style.cssText = 'border: solid; border-color: black; width: 50px; height: 50px';
-    div2.classList.add('box2');
-    div.appendChild(div2); 
-} */
