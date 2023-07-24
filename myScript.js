9f851909c463fcc54b625023e0559b1efc856a6d
Tryndameir <andrewdinho99@gmail.com>
2023-07-24 04:35:18 -0400

    addedd css to grid

--- myScript.js
+++ myScript.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.container');
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
     const div = document.createElement('div');
-    div.style.cssText = 'border: solid; border-color: black; width: 50px; height: 50px';
+    div.style.cssText = 'border: solid; border-width: 1px; border-color: black; width: 50px; height: 50px';
     div.classList.add('box');
     container.appendChild(div);
     }
