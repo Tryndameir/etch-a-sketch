ebfa315b13580bc8a9bacdb63fcff3d383984ec1
Tryndameir <andrewdinho99@gmail.com>
2023-07-24 03:01:15 -0400

    added a box with black border with js

---
+++ myScript.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+container.appendChild(div);
+div.style.cssText = 'border: solid; border-color: black; width: 50px; height: 50px';
