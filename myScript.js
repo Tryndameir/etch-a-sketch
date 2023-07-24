1c0dcccc33789cff84ec04c8a274d4d2e035c9ad
Tryndameir <andrewdinho99@gmail.com>
2023-07-24 04:16:36 -0400

    created 16x16 total grid but not 16x16 visually

--- myScript.js
+++ myScript.js
@@ -1,4 +1,17 @@
 const container = document.querySelector('.container');
-const div = document.createElement('div');
-container.appendChild(div);
-div.style.cssText = 'border: solid; border-color: black; width: 50px; height: 50px';
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+    const div = document.createElement('div');
+    div.style.cssText = 'border: solid; border-color: black; width: 50px; height: 50px';
+    div.classList.add('box');
+    container.appendChild(div);
+    }
+}
+
+/*const div = document.querySelector('.box');
+for (let i = 0; i < 16; i++) {
+    const div2 = document.createElement('div');
+    div2.style.cssText = 'border: solid; border-color: black; width: 50px; height: 50px';
+    div2.classList.add('box2');
+    div.appendChild(div2); 
+} */
