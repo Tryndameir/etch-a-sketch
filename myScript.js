f38cff6044766fc29f3c8e3e07a30d296dbe5d05
Tryndameir <andrewdinho99@gmail.com>
2023-07-27 19:52:30 -0400

    fixed issue where no color change after grid reformed

--- myScript.js
+++ myScript.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 let canvasSize = document.querySelector('#btn1');
 const color = document.querySelector('#btn2');
 let box = document.createElement('div');
+box.style.cssText = 'border: solid; border-width: 1px; border-color: black; background-color: white';
 //const column = document.createElement('div');
 //column.classList.add('box');
 //const row = document.createElement('div'); //create a div called boxes
@@ -11,11 +12,10 @@ let size = 16;
 for (let i = 0; i < size * size; i++) { // loop to generate canvas
     let box = document.createElement('div');
     box.classList.add('box');
-    box.style.cssText = 'border: solid; border-width: 1px; border-color: black';
+    box.style.cssText = 'border: solid; border-width: 1px; border-color: black; background-color: white';
     let borderOffset = size * 2;
     box.style.width = ((832 - borderOffset) / size) + "px";
     container.appendChild(box);
-    console.log(size);
 }
 
 canvasSize.addEventListener('click', () => { //event listener for when user clicks canvasSize botton
@@ -32,29 +32,43 @@ canvasSize.addEventListener('click', () => { //event listener for when user clic
             for (let i = 0; i < size * size; i++) { // loop to generate canvas
                 let box = document.createElement('div');
                 box.classList.add('box');
-                box.style.cssText = 'border: solid; border-width: 1px; border-color: black';
+                box.style.cssText = 'border: solid; border-width: 1px; border-color: black; background-color: white';
                 let borderOffset = size * 2;
                 box.style.width = ((832 - borderOffset) / size) + "px";
                 container.appendChild(box);
             }
-            
-        /////////////////////////////////////////////////////////////////////////////////////////////////
-
-});
-
-
-box = document.querySelectorAll('.box');
-
-box.forEach(box => { // event listener for when user clicks inside canvas to sketch
-    box.style.backgroundColor = 'white';
+            box = document.querySelectorAll('.box');
+    box.forEach(box => { // event listener for when user clicks inside canvas to sketch
+    //box.style.backgroundColor = 'white';
     box.addEventListener('click', () => {
         if (box.style.backgroundColor == 'red') {
-            box.style.backgroundColor = 'white';
+            box.style.backgroundColor = 'green';
         }
 
         else {
         box.style.backgroundColor = 'red';
         }    
+        console.log('clicks');
+    });
+});
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+
+});
+
+
+
+box = document.querySelectorAll('.box');
+box.forEach(box => { // event listener for when user clicks inside canvas to sketch
+    //box.style.backgroundColor = 'white';
+    box.addEventListener('click', () => {
+        if (box.style.backgroundColor == 'red') {
+            box.style.backgroundColor = 'green';
+        }
+
+        else {
+        box.style.backgroundColor = 'red';
+        }    
+        console.log('clicks');
     });
 });
 
